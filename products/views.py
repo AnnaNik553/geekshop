f06ef846f8846b6c900c61897f1f266c5f27e4c6
Anna Nikiforyak <a.nikiforyak@gmail.com>
2021-06-01 22:54:22 +0300

    added HW for lesson2

--- products/views.py
+++ products/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
+import os
+import json
+
+MODULE_DIR = os.path.dirname(__file__)
 
 
 def index(request):
-    return render(request, 'products/index.html')
+    context = {
+        'title': 'GeekShop',
+    }
+    return render(request, 'products/index.html', context)
 
 
 def products(request):
-    return render(request, 'products/products.html')
+    context = {'title': 'GeekShop - Каталог'}
+    file_path = os.path.join(MODULE_DIR, 'fixtures/goods.json')
+    context['products'] = json.load(open(file_path, encoding='UTF-8'))
+    return render(request, 'products/products.html', context)
